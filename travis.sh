6f713fbd91fea7cf78f4fc78acc4024d3d900c6f
JDGrimes <jdg@codesymphony.co>
2016-07-04 17:57:05 -0400

    Actually install WordPress
    While we’re at it, let’s just use the `wp-config.php` file, there is no
    need to use the `wp-tests-config.php` file, that is just leftover cruft
    from where I copied this from. We still need to copy the sample tests
    config though, since some things defined in there are needed during
    install.

--- travis.sh
+++ travis.sh
@@ -8,22 +8,24 @@ install-wordpress() {
 	# Clone the WordPress develop repo.
 	git clone --depth=1 --branch="$WP_VERSION" git://develop.git.wordpress.org/ "$WP_DEVELOP_DIR"
 
-	# Set up tests config.
 	cd "$WP_DEVELOP_DIR"
-	cp wp-tests-config-sample.php wp-tests-config.php
-	sed -i "s/youremptytestdbnamehere/wordpress_test/" wp-tests-config.php
-	sed -i "s/yourusernamehere/root/" wp-tests-config.php
-	sed -i "s/yourpasswordhere//" wp-tests-config.php
-	cd -
+
+	# Set up tests config.
+	cp wp-tests-config-sample.php wp-config.php
+	sed -i "s/youremptytestdbnamehere/wordpress_test/" wp-config.php
+	sed -i "s/yourusernamehere/root/" wp-config.php
+	sed -i "s/yourpasswordhere//" wp-config.php
 
 	# Set up database.
 	mysql -e 'CREATE DATABASE wordpress_test;' -uroot
 
 	# Configure WordPress for access through a web server.
-	cd "$WP_DEVELOP_DIR"
-	sed -i "s/example.org/$WP_CEPT_SERVER/" wp-tests-config.php
-	cp wp-tests-config.php wp-config.php
+	sed -i "s/example.org/$WP_CEPT_SERVER/" wp-config.php
 	echo "require_once(ABSPATH . 'wp-settings.php');" >> wp-config.php
+
+	# Install.
+	php tests/phpunit/includes/install.php wp-config.php
+
 	cd -
 }
 
